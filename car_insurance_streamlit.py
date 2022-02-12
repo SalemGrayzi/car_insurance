6c5a1e64fb0872db42be54253c983b0a3874157a
SalemGrayzi <99489268+SalemGrayzi@users.noreply.github.com>
2022-02-12 14:22:06 +0200

    Update car_insurance_streamlit.py

--- car_insurance_streamlit.py
+++ car_insurance_streamlit.py
@@ -77,73 +77,8 @@ fig3.update_layout(
     yaxis_title="Claimed Insurance",
     legend_title="Gender")
 
-vy=df['VEHICLE_YEAR'].value_counts()
-vys=df['VEHICLE_YEAR'].value_counts().index
-fig4=px.pie(values=vy,names=vys)
-fig4.update_layout(
-    title="Percentage of Vehicle Age",
-    legend_title="Age of Vehicle")
-fig4.update_traces(textinfo = 'percent+label')
-
-fig5 = px.violin(df, x="OUTCOME", y="ANNUAL_MILEAGE",box=True,color='GENDER')
-fig5.update_layout(
-    title="Annual Mileage for Claiming Insurance",
-    xaxis_title="Not Claimed or Claimed Insurance",
-    yaxis_title="Annual Mileage",
-    legend_title="Gender")
-
-fig6=px.pie(values=df['DUIS'].value_counts().values,names=df['DUIS'].value_counts().index)
-fig6.update_layout(
-    title="Percentage of DUIS Offense",
-    legend_title="Number/s of Offense")
-fig6.update_traces(textinfo = 'percent+label')
-
-pa=df['PAST_ACCIDENTS'].value_counts().nlargest(7)
-pas=df['PAST_ACCIDENTS'].value_counts().nlargest(7).index
-fig7=px.pie(values=pa,names=pas)
-fig7.update_layout(
-    title="Percentage of Past Accidents",
-    legend_title="Number/s of Past Accidents")
-fig7.update_traces(textinfo = 'percent+label')
-
-pv=df['SPEEDING_VIOLATIONS'].value_counts().nlargest(7)
-pvs=df['SPEEDING_VIOLATIONS'].value_counts().nlargest(7).index
-fig8=px.pie(values=pv,names=pvs)
-fig8.update_layout(
-    title="Percentage of Speeding Offense",
-    legend_title="Number/s of Offense")
-fig8.update_traces(textinfo = 'percent+label')
-
-fig9 = go.Figure()
-fig9.add_trace(go.Histogram(x=df['OUTCOME'],y=df['SPEEDING_VIOLATIONS'],histfunc="avg",name="SPEEDING_VIOLATIONS"))
-fig9.add_trace(go.Histogram(x=df['OUTCOME'],y=df['PAST_ACCIDENTS'],histfunc="avg",name="PAST_ACCIDENTS"))
-fig9.add_trace(go.Histogram(x=df['OUTCOME'],y=df['DUIS'],histfunc="avg",name="DUIS"))
-fig9.update_layout(
-    title="Does Violation Increase Claiming Insurance?",
-    xaxis_title="Violation of Law",
-    yaxis_title="Claimed Insurance",
-    legend_title="Violation Type")
-
-fig10 = px.histogram(df, x=df['RACE'],y=df['OUTCOME'],histfunc="sum",color='INCOME', barmode='group',text_auto='.2s')
-fig10.update_layout(
-    title="Does Income Affect Claiming Insurance?",
-    xaxis_title="Race",
-    yaxis_title="Claimed Insurance",
-    legend_title="Income Group")
-
-fig11 = px.icicle(
-    df,
-    path= [px.Constant("Distribution of Customers"),'AGE','INCOME',"RACE"],
-    values="OUTCOME",color_continuous_scale='Edge')
-
 chart_visual = st.sidebar.selectbox('Select Charts/Plot type',
-                                    ['All','Gender and Vehicle', 'Percentage of Vehicle Type',
-                                    'Age of Vehicle','Percentage of Vehicle Age',
-                                    'Annual Mileage','Percentage of DUIS Offense',
-                                    'Percentage of Past Accidents',
-                                    'Percentage of Speeding Offense',
-                                    'Does Violation Increase Claiming Insurance?',
-                                    'Income Group','Distribution of Customers'])
+                                    ['All','Gender and Vehicle', 'Percentage of Vehicle Type'])
 
 if chart_visual == 'Gender and Vehicle':
         fig1
@@ -151,23 +86,6 @@ elif chart_visual == 'Percentage of Vehicle Type':
         fig2
 elif chart_visual == 'Age of Vehicle':
         fig3
-elif chart_visual == 'Percentage of Vehicle Age':
-        fig4
-elif chart_visual == 'Annual Mileage':
-        fig5
-elif chart_visual == 'Percentage of DUIS Offense':
-        fig6
-elif chart_visual == 'Percentage of Past Accidents':
-        fig7
-elif chart_visual == 'Percentage of Speeding Offense':
-        fig8
-elif chart_visual == 'Does Violation Increase Claiming Insurance?':
-        fig9
-elif chart_visual == 'Income Group':
-        fig10
-elif chart_visual == 'Distribution of Customers':
-        fig11
-elif chart_visual == 'All':
-        fig1,fig2,fig3,fig4,fig5,fig6,fig7,fig8,fig9,fig10,fig11
+
 
 st.sidebar.write("You selected", chart_visual)
